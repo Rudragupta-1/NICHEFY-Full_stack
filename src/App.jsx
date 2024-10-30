30ceb968656364c616187bbc088500911940c6ec
Rudra Gupta <guptarudra901@gmail.com>
2024-10-30 23:52:17 +0530

    shadcn installation completed
    using shadcn for our UI

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { Button } from './components/ui/button'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <Button>Login</Button>
+  )
+}
+
+export default App
